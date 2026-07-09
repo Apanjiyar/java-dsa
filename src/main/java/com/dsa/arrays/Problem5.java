31311aa0eb92b5b8996a690e7bc408b05200f30f
arun-panjiyar <arun.panjiyar@sprouts.ai>
2026-07-09 12:00:53 +0530

    update

---
+++ src/main/java/com/dsa/arrays/Problem5.java
@@ -0,0 +1,10 @@
+package com.dsa.arrays;
+
+/**
+ * Move all the negative elements to one side of the array
+ * https://www.geeksforgeeks.org/problems/move-all-negative-elements-to-end1813/1
+ * https://www.geeksforgeeks.org/problems/find-minimum-and-maximum-element-in-an-array4428/1
+ */
+public class Problem5 {
+    
+}
