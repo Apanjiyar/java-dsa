12bb059a6857d2fb71703fcd68384ab345c0c4ae
arun-panjiyar <arun.panjiyar@sprouts.ai>
2026-07-13 12:45:54 +0530

    updated

--- src/main/java/com/dsa/arrays/Temp.java
+++ src/main/java/com/dsa/arrays/Temp.java
@@ -3,26 +3,6 @@ package com.dsa.arrays;
 public class Temp {
 
     public static void main(String[] args){
-        int[] nums1 = {0};
-        segregate0and1()
 
-    }
-
-    void segregate0and1(int[] arr) {
-        // code here
-        int left = 0;
-        int right = 0;
-        while (left <= right) {
-            if(arr[left] == 1 && arr[right] == 0) {
-                arr[left] = 0;
-                arr[right] = 1;
-                left++; right--;
-            }
-            else if(arr[left] == 0) {
-                left++;
-            } else if(arr[right] == 1) {
-                right--;
-            }
-        }
     }
 }
